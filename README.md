1bd60010867fde886e6f2da6b9b9bcce332af29a
Matteo Sacchetto <matteosacchetto1996@gmail.com>
2019-12-11 09:34:26 +0100

    Added http and https modules

--- README.md
+++ README.md
@@ -6,10 +6,39 @@
 
 ## Modules
 
-* [express](https://expressjs.com/en/4x/api.html)
+### Express
+
+Module used to create a server/RESTful API
+
+* Version: 4.17.1
+
+* Website: https://expressjs.com/en
+
+* Documentation: https://expressjs.com/en/4x/api.html
+
+
+
+### Http and Https
+
+Modules used to explicitly create a HTTP and HTTPS server
+
+* Enviroment variables used:
+  * process.env.PORT: to specify the HTTP 
+  * process.env.PORT_HTTPS: to specify the HTTPS
+* Version: NodeJS version
+* Documentation: 
+  * HTTP: https://nodejs.org/api/http.html
+  * HTTPS: https://nodejs.org/api/https.html
 
 
 
 ## Dev modules
 
-* [nodemon](https://github.com/remy/nodemon#nodemon)
+### Nodemon
+
+Module used to auto-reload the app when changes are detected
+
+* Website: https://nodemon.io/
+
+* Documentation: https://github.com/remy/nodemon#nodemon
+
--- app.js
+++ app.js
@@ -1,12 +1,46 @@
+// Modules
 const express = require('express')
+const http = require('http');
+const https = require('https');
 
+// App settings
 const app = express()
-const port = process.env.PORT || 3000
+const httpPort = process.env.PORT || 80
+const httpsPort = process.env.PORT_HTTPS || 443
+
+// Options
+const opt_http = true
+const opt_https = false
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}!`)
-})
+if(opt_http) {
+  // Starting http server
+  const httpServer = http.createServer(app);
+
+  httpServer.listen(httpPort, () => {
+  	console.log(`HTTP Server running on port ${httpPort}`);
+  });
+}
+
+if(opt_https) {
+  // Certificate
+  const privateKey = fs.readFileSync('key.pem', 'utf8');
+  const certificate = fs.readFileSync('cert.pem', 'utf8');
+  const ca = fs.readFileSync('chain.pem', 'utf8');
+
+  const credentials = {
+  	key: privateKey,
+  	cert: certificate,
+  	ca: ca
+  };
+
+  // Starting https server
+  const httpsServer = https.createServer(credentials, app);
+
+  httpsServer.listen(httpsPort, () => {
+  	console.log(`HTTPS Server running on port ${httpsPort}`);
+  });
+}
