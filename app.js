d2c1d3c2435c12c5822cb48f35028847c73f6ecc
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2021-05-10 12:52:50 +0200

    Update template (#24)
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    
    * Solved security vulnerability, removed explicit body-parser, added hpp and updated app.js
    
    * Updated README.md
    
    * Updated template
    
    Updated .gitignore, package.json. Added config module and updated logger and app.js. Updated README.md
    
    * Updated server to handle SPA
    
    * Updated server with some custom middlewares
    
    Added a middleware to handle HTTP response status based on response body, added a middleware to send 404 on /api or /, renamed utils to http-utils, and some minor fixes
    
    * Updated README.md
    
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>

--- app.js
+++ app.js
@@ -6,89 +6,103 @@ const fs = require('fs')
 const morgan = require('morgan')
 const helmet = require('helmet')
 const compression = require('compression')
-const bodyParser = require('body-parser')
 const httpStatusCodes = require('http-status-codes')
 const rateLimit = require("express-rate-limit")
+const hpp = require('hpp')
 const dotenv = require('dotenv')
+
+// Load App settings
+const config =  require('./lib/config.js')
 
 // Custom modules
 const logger = require('./lib/logger.js')
-const utils = require('./lib/utils.js')
+const httpUtils = require('./lib/http-utils.js')
+
+// Custom middlewares
+const responseStatus = require('./middlewares/response-status.js');
+const apiNotFound = require('./middlewares/api-not-found');
 
 // Custom routes
 const apiRoute = require('./routes/api.js')
 
-// Loading App Settings
-dotenv.config()
-const httpPort = process.env.PORT || 8000
-const httpsPort = process.env.PORT_HTTPS || 44300
-const environment = process.env.NODE_ENV || 'development'
 const limiter = rateLimit({
-  windowMs: 10 * 60 * 1000, // 10 minutes
+  windowMs: 1 * 60 * 1000, // 1 minute
   max: 250, // limit each IP to 250 requests per windowMs
-  message: utils.createResponse(429)
+  message: httpUtils.createResponse(429)
 })
-
-// Options
-const opt_http = true
-const opt_https = false
-const opt_static = false
-const opt_reverse_proxy = false
 
 // Create app
 const app = express()
 
-// Define middlewares
-app.use(limiter) // Apply the limit to all requests
-app.use(bodyParser.urlencoded({ extended: false })) // Parse application/x-www-form-urlencoded
-app.use(bodyParser.json()) // Parse application/json
-app.use(helmet())
-
-if(environment === 'development') {
+// Define logging middlewares
+if(config.environment === 'development') {
   // Middlewares used in development
   app.use(morgan('tiny', { stream: logger.stream }))
 }
-else if(environment === 'production') {
+else if(config.environment === 'production') {
   // Middlewares used in production
   app.use(morgan('combined', { stream: logger.stream }))
   app.use(compression())
 }
 
-// Define routes -> TODO: write here your code
-app.use('/api', apiRoute)
+// Define middlewares
+app.use(express.urlencoded({ extended: true })) // Parse application/x-www-form-urlencoded
+app.use(express.json()) // Parse application/json
+app.use(helmet())
+app.use(hpp())
 
-if(opt_static) {
+// Define API middlewares
+app.use('/api', limiter); // Apply the limit to all API requests
+app.use('/api', responseStatus); // HTTP response status is coherent with message status
+
+// Define routes -> TODO: write here your code
+app.use('/api/v1', apiRoute)
+
+if(config.useStatic) {
   // Define static folder
-  app.use(express.static('public'))
+  app.use(express.static(config.staticFolder))
+
+  if(config.serveSPA) {
+    // We want to server a Single Page Application (SPA)
+    app.get('*', (req, res) => {
+      res.sendFile('index.html', { root: config.staticFolder });
+    });
+  }
+  
+  // Handle '*' on api
+  app.use('/api', apiNotFound); // If a request for the /api/* has not been served => return 404
+}
+else {
+  // Handle '*' on /
+  app.use('/', apiNotFound); // If a request for the /* has not been served => return 404
 }
 
-if(opt_http) {
+
+if(config.useHttp) {
   // Starting http server
   const httpServer = http.createServer(app);
 
   // It listens on port httpPort
-  httpServer.listen(httpPort, () => {
-  	logger.info(`HTTP Server running on port ${httpPort}`)
+  httpServer.listen(config.httpPort, () => {
+  	logger.info(`HTTP Server running on port ${config.httpPort}`)
   })
 }
 
-if(opt_https) {
+if(config.useHttps) {
   // Certificate
-  const privateKey = fs.readFileSync('key.pem', 'utf8')
-  const certificate = fs.readFileSync('cert.pem', 'utf8')
-  const ca = fs.readFileSync('chain.pem', 'utf8')
+  const privateKey = fs.readFileSync(config.sslKeyPath, 'utf8')
+  const certificate = fs.readFileSync(config.sslCertPath, 'utf8')
 
   const credentials = {
   	key: privateKey,
-  	cert: certificate,
-  	ca: ca
+  	cert: certificate
   }
 
   // Starting https server
   const httpsServer = https.createServer(credentials, app)
 
   // It listens on port httpsServer
-  httpsServer.listen(httpsPort, () => {
-  	logger.info(`HTTPS Server running on port ${httpsPort}`)
+  httpsServer.listen(config.httpsPort, () => {
+  	logger.info(`HTTPS Server running on port ${config.httpsPort}`)
   })
 }
--- routes/api.js
+++ routes/api.js
@@ -1,37 +1,32 @@
 // Node modules
 const express = require('express')
 const root = require('app-root-path')
-const httpStatusCodes = require('http-status-codes')
 
 // Custom modules
 const logger = require(`${root}/lib/logger.js`)
-const utils = require(`${root}/lib/utils.js`)
+const httpUtils = require(`${root}/lib/http-utils.js`)
 
 // Create route
 const router = express.Router()
 
-// Handle /api routes -> TODO: write here your code
+// Handle /api/v1 routes -> TODO: write here your code
 router
   .route('/')
   .get((req, res, next) => {
     res
-      .status(200)
-      .send(utils.createResponse(200,{ method: req.method }))
+      .send(httpUtils.createResponse(200,{ method: req.method }))
   })
   .post((req, res, next) => {
     res
-      .status(200)
-      .send(utils.createResponse(200,{ method: req.method }))
+      .send(httpUtils.createResponse(200,{ method: req.method }))
   })
   .put((req, res, next) => {
     res
-      .status(200)
-      .send(utils.createResponse(200,{ method: req.method }))
+      .send(httpUtils.createResponse(200,{ method: req.method }))
   })
   .delete((req, res, next) => {
     res
-      .status(200)
-      .send(utils.createResponse(200,{ method: req.method }))
+      .send(httpUtils.createResponse(200,{ method: req.method }))
   })
 
 // Export the router
