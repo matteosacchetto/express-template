f2f38362716cee903a97e9f2cf110b2f12707091
Matteo Sacchetto <56300116+matteosacchetto@users.noreply.github.com>
2021-05-13 10:20:07 +0200

    Ensure code consistency

--- middlewares/response-status.js
+++ middlewares/response-status.js
@@ -1,12 +1,12 @@
-const mung = require('express-mung');
+const mung = require('express-mung')
 
 const responseStatus = mung.json((body, req, res) => {
   if (body.statusCode) {
     // Set response status code
-    res.status(body.statusCode);
+    res.status(body.statusCode)
   }
 
-  return body;
-});
+  return body
+})
 
-module.exports = responseStatus;
+module.exports = responseStatus
